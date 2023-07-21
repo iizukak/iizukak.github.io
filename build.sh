4a724f1865c66c1ffee27d2a507b77e51cdf531b
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2023-07-21 21:44:46 +0900

    Small refactor

--- build.sh
+++ build.sh
@@ -7,9 +7,10 @@ pdfTargets=("cv" "resume")
 
 for target in ${pdfTargets[@]}; do
     pandoc ./markdown/${target}.md -o docs/${target}.pdf \
-        -V documentclass=ltjsarticle \
         --pdf-engine=lualatex \
+        -V documentclass=ltjsarticle \
         -V classoption=pandoc \
         -V block-headings \
+        -V fontsize=10pt \
         -V luatexjapresetoptions=hiragino-pron
 done
